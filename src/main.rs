9c808ee6bb32568473edb853850787b6245fc150
Cypher402 <code@code.philasky.com>
2022-11-03 14:08:04 -0700

    Added type_transfer function to show type inheritance

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
+fn type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
 fn main() {
     let x = "Hello";
     let y = 42;
     let var = 42.69;
     let b = true;
 
-    TypeOf(&x);
-    TypeOf(&y);
-    TypeOf(&var);
-    TypeOf(&b);
+    type_of(&x);
+    type_of(&y);
+    type_of(&var);
+    type_of(&b);
+
+    type_transfer();
 }
 
-fn TypeOf<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>());
+fn type_transfer() {
+    let vartan: u8 = 3;
+    type_of(&vartan);
+    let narton = vartan;
+    type_of(&narton);
+    type_of(&vartan);
+    println!("The Vars  {} {}", vartan, narton);
 }
